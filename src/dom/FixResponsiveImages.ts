72007bd907070ff03101aa21a2a89de70b1aceb7
Justin Kimbrell <jkimbrell@actengag.com>
2023-08-05 18:54:52 -0600

    fix: issue with responsive images breaking

--- src/dom/FixResponsiveImages.ts
+++ src/dom/FixResponsiveImages.ts
@@ -22,57 +22,19 @@ export default class FixResponsiveImages extends BaseDomPlugin<FixResponsiveImag
                 continue;
             }
 
-            const { unit, value } = units.parse($el.css('width'));
+            const { unit, value } = units.parse($el.css('width') ?? 0);
 
             if(unit === '%') {
                 $el.attr('width', String(this.options.maxWidth * (100 / value)));
             }
-            else if($el.css('width')) {
+            else if(value) {
                 $el.attr('width', units.convert('px', `${value}${unit || 'px'}`));
             }
                 
-            if(!$el.css('width')) {
+            if(!$el.css('width') && $el.attr('width')) {
                 $el.css('width', `${$el.attr('width')}px`);
             }
         }
-
-        // $('img').each((i, el) => { 
-        //     const $el = $(el);
-
-        //     if(!$el.attr('width')) {
-        //         const width = getComputedStyle(el, '').width;
-        //         const { unit, value } = parse(width);
-
-        //         if(unit === '%') {
-        //             $el.attr('width', this.options.maxWidth * (100 / value));
-        //         }
-        //         else if(width) {
-        //             $el.attr('width', convert('px', `${value}${unit || 'px'}`, el));
-        //         }
-        //     }
-        //     else if(!$el.css('width')) {
-        //         $el.css('width', `${$el.attr('width')}px`);
-        //     }
-        // });
-
-        /*
-        Replaced: 
-        document.querySelectorAll('img').forEach(el => {        
-            if(!el.getAttribute('width')) {
-                const widthSuffix = suffix(el.style.width);
-
-                if(widthSuffix === '%') {
-                    el.setAttribute('width', maxWidth * (100 / convertUnitToFloat(el.style.width)));
-                }
-                else if(widthSuffix === 'px') {
-                    el.setAttribute('width', Math.min(maxWidth, convertUnitToFloat(el.style.width)));
-                }
-            }
-            else if(!el.style.width) {
-                el.style.width = unit(el.getAttribute('width'));
-            }
-        });
-        */
 
         return $;
     }

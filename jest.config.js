c0366a81f2d9c54e205cbd6974fddc2ec733a0fc
Justin Kimbrell <jkimbrell@actengag.com>
2023-08-14 20:10:28 -0600

    chore: cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 export default {
     testEnvironment: 'node',
     testMatch: [
-        "**/__tests__/**/*.+(ts|tsx|js)",
-        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+        '**/__tests__/**/*.+(ts|tsx|js)',
+        '**/?(*.)+(spec|test).+(ts|tsx|js)'
     ],
     transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-        "^.+\\.js$": "babel-jest"
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.js$': 'babel-jest'
     },
     transformIgnorePatterns: [
-        "node_modules/(?!(html-chunk)/)"
+        'node_modules/(?!(html-chunk)/)'
     ]
 };

9845352784856667e0a6297ba483200e2c2aa664
Justin Kimbrell <jkimbrell@actengag.com>
2025-07-16 09:43:29 -0600

    chore: removed dev deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './demo/**/*.vue',
-        './node_modules/@vue-interface/**/src/*.{vue,ts}',
-    ],
-    presets: [
-        require('@vue-interface/form-control/tailwindcss')
+        './demo/**/*.vue'
     ]
 };
 
